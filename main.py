e87426668259f4fe0ff7ac1c177cccd99350d75b
Chaemin-Lim <antraxmin@naver.com>
2024-02-19 21:29:39 +0900

    setting

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
-app.mount("/", StaticFiles(directory="public", html = True), name="static")
+# app.mount("/", StaticFiles(directory="public", html = True), name="static")
+
